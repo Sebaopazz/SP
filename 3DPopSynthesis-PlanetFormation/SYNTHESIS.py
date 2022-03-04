70ef9aa282c1407a337392f5b3b3f58a69a6b2d5
Sebaopazz <97682662+Sebaopazz@users.noreply.github.com>
2022-03-05 00:25:16 +0100

    Add files via upload

---
+++ 3DPopSynthesis-PlanetFormation/SYNTHESIS.py
@@ -0,0 +1,15 @@
+import sys
+
+import Synthesis.pre as pre
+
+if __name__ == "__main__":
+    Name = sys.argv[1]
+    N = sys.argv[2]
+    RUNTIME = sys.argv[3]
+    EVOTIME = sys.argv[4]
+
+    # Name = 'Test'
+    # N = 5
+    cmd,rst = pre.setup(Name, N, RUNTIME, EVOTIME)
+    # print(cmd)
+    # print(rst)
